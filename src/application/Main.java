6d79f39425ca5c4862dfe9c9e6a3f578cc667944
Bruno Garcia <brunodog@gmail.com>
2018-09-30 11:23:37 -0300

    exercicio da aula 46 concluido

--- src/application/Main.java
+++ src/application/Main.java
@@ -8,16 +8,31 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /* RESOLUCAO EXERCICIO 46
         double moeda;
         System.out.print("Digite a Cotação do dolar: ");
         Cotacao.dolar = sc.nextDouble();
         System.out.print("Valor em BRL: ");
         moeda = sc.nextDouble();
         System.out.println("Valor em USD: "+Cotacao.conversaoReal(moeda));
+        */
+
 
 
     }
